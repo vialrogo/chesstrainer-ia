d5c3bb3b46e6e43a46a6d3869b3d931dd5ccd90d
vialrogo@gmail.com <vialrogo@gmail.com@d3711144-6350-d657-4f93-78f5ff1c1195>
2012-01-09 22:00:12 +0000

    Comienza la interfaz. Se agrega Ventana y Main y se arregla el .pro que estaba malo (no dejaba agregar archivos)

---
+++ trunk/Ventana.cpp
@@ -0,0 +1,30 @@
+#include "Ventana.h"
+#include "ui_Ventana.h"
+
+Ventana::Ventana(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::Ventana)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("Project ChessTrainer-IA");
+
+    connect(ui->actionQuit, SIGNAL(triggered()),this,SLOT(close()));
+    connect(ui->actionAbout,SIGNAL(triggered()),this,SLOT(acercaDe()));
+}
+
+Ventana::~Ventana()
+{
+    delete ui;
+}
+
+void Ventana::acercaDe()
+{
+    QMessageBox msgBox;
+    msgBox.setTextFormat(Qt::RichText);
+    msgBox.setWindowTitle("About");
+    msgBox.setText("<h3>Project ChessTrainer-IA</h3>");
+    msgBox.setInformativeText("Create by: \n    Victor Alberto Romero\n    Erika Suarez Valencia");
+    msgBox.setIcon(QMessageBox::Information);
+    msgBox.exec();
+    return;
+}
