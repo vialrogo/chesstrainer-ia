1574f2cdea6cc3ec8c531e7edfafb5d35c9352bb
vialrogo@gmail.com <vialrogo@gmail.com@d3711144-6350-d657-4f93-78f5ff1c1195>
2012-01-17 22:45:43 +0000

    Implementado el método sePuedeMover

--- trunk/Minimax.h
+++ trunk/Minimax.h
@@ -18,15 +18,12 @@ private:
 
     Mapa mapa;
 
-    char sePuedeMover(int x, int y);
-
 public:
     MiniMax(int nivelIn);
     string tomarDesicion();
     void definirVariables(char **estadoIn, int *posBlancasXIn, int *posBlancasYIn, int *posNegrasXIn, int *posNegrasYIn);
     bool verificarJaque(bool color);
-    bool sePuedeMover();
-
+    int sePuedeMover(int xDestino, int yDestino, int ficha, bool color);
 };
 
 #endif // MINIMAX_H
