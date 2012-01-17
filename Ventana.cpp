3e3badf8e6a407b2f9a4479dee08b14a3d884f9a
vialrogo@gmail.com <vialrogo@gmail.com@d3711144-6350-d657-4f93-78f5ff1c1195>
2012-01-17 02:33:07 +0000

    Se mueve el método verificarJaque a la clase Minimax (haciendo todos los cambios necesarios en variables y demás). Se convierte el método de los botones Easy, Medium y Hard en uno sólo (es una forma un poco machetera, pero es la única manera de hacerlo)

--- Ventana.cpp
+++ Ventana.cpp
@@ -29,6 +29,7 @@ Ventana::Ventana(QWidget *parent) :
     ui->graphicsView->setScene(tablerito);
     ui->graphicsView->setHidden(true);
 
+
     connect(ui->actionQuit, SIGNAL(triggered()),this,SLOT(close()));
     connect(ui->actionAbout,SIGNAL(triggered()),this,SLOT(acercaDe()));
     connect(ui->actionNew_Game,SIGNAL(triggered()),this,SLOT(newGame()));
@@ -90,6 +91,7 @@ void Ventana::crearTablero()
     posBlancasY[7]=ptmp.ry();
     vect.remove(tmp);
     crearEstadoDeArreglos();
+    minimax->definirVariables(estado,posBlancasX,posBlancasY,posNegrasX,posNegrasY);
 
     //Coloco Rey Negro
     while(true)
@@ -100,8 +102,9 @@ void Ventana::crearTablero()
         posNegrasY[7]= ptmp.ry();
         vect.remove(tmp);
         crearEstadoDeArreglos();
+        minimax->definirVariables(estado,posBlancasX,posBlancasY,posNegrasX,posNegrasY);
 
-        if(verificarJaque(true))
+        if(minimax->verificarJaque(true))
             vect.append(ptmp);
         else
             break;
@@ -119,8 +122,9 @@ void Ventana::crearTablero()
         posBlancasY[i]= ptmp.ry();
         vect.remove(tmp);
         crearEstadoDeArreglos();
+        minimax->definirVariables(estado,posBlancasX,posBlancasY,posNegrasX,posNegrasY);
 
-        if(verificarJaque(false))
+        if(minimax->verificarJaque(false))
             vect.append(ptmp);
         else
             i++;
@@ -138,8 +142,9 @@ void Ventana::crearTablero()
         posNegrasY[i]= ptmp.ry();
         vect.remove(tmp);
         crearEstadoDeArreglos();
+        minimax->definirVariables(estado,posBlancasX,posBlancasY,posNegrasX,posNegrasY);
 
-        if(verificarJaque(true))
+        if(minimax->verificarJaque(true))
             vect.append(ptmp);
         else
             i++;
@@ -157,152 +162,6 @@ void Ventana::crearTablero()
     tablerito->pintarCuadricula();
     tablerito->crearFichas(posBlancasX,posBlancasY,posNegrasX,posNegrasY);
     tablerito->setEstado(estado);
-}
-
-bool Ventana::verificarJaque(bool color) //color=false => negras / color=true => blancas
-{
-    /*
-        B = 66    b = 98
-        C = 67    c = 99
-        K = 75    k = 107
-        P = 80    p = 112
-        Q = 81    q = 113
-     */
-
-    int delta = color? 32 : 0;
-
-    int reyX = color? posBlancasX[7] : posNegrasX[7];
-    int reyY = color? posBlancasY[7] : posNegrasY[7];
-
-    int dxDiago[] = {-1, 1,-1, 1};
-    int dyDiago[] = {-1,-1, 1, 1};
-
-    int dxRectas[] = { 0, 1, 0,-1};
-    int dyRectas[] = {-1, 0, 1, 0};
-
-    int dxCaballo[] = {-2,-2,-1,-1, 1, 1, 2, 2};
-    int dyCaballo[] = {-1, 1,-2, 2,-2, 2,-1, 1};
-
-    int dxRey[] = {-1,-1,-1, 0, 0, 1, 1, 1};
-    int dyRey[] = {-1, 0, 1,-1, 1,-1, 0, 1};
-
-    int idx=0;
-    int count=1;
-    int xx = 0;
-    int yy = 0;
-    bool respuesta = false;
-
-    //alfiles-reinaDiagonal
-    idx=0;
-    count=1;
-    while (true)
-    {
-        if(idx > 3) break;
-
-        xx = reyX+(count*dxDiago[idx]);
-        yy = reyY+(count*dyDiago[idx]);
-
-        if(xx<0 || xx>5 || yy<0 || yy>5)
-        {
-            count=1;
-            idx++;
-        }
-        else
-        {
-            if(estado[yy][xx] == ' ')
-                count++;
-            else{
-                if(estado[yy][xx] == (81+delta) || estado[yy][xx] == (66+delta))
-                    return true;
-                else
-                {
-                    count=1;
-                    idx++;
-                }
-            }
-        }
-    }
-
-    //ReinaColumna
-    idx=0;
-    count=1;
-    while (true)
-    {
-        if(idx > 3) break;
-
-        xx = reyX+(count*dxRectas[idx]);
-        yy = reyY+(count*dyRectas[idx]);
-
-        if(xx<0 || xx>5 || yy<0 || yy>5)
-        {
-            count=1;
-            idx++;
-        }
-        else
-        {
-            if(estado[yy][xx] == ' ')
-                count++;
-            else{
-                if(estado[yy][xx] == (81+delta))
-                    return true;
-                else
-                {
-                    count=1;
-                    idx++;
-                }
-            }
-        }
-    }
-
-    //Caballos
-    for (int i = 0; i < 8; ++i)
-    {
-        xx = reyX + dxCaballo[i];
-        yy = reyY + dyCaballo[i];
-
-        if(xx<0 || xx>5 || yy<0 || yy>5)
-            continue;
-        else
-            if(estado[yy][xx] == (67+delta))
-                return true;
-    }
-
-    //Peones
-    if(color)
-    {
-        if(reyX>0 && reyY>0)
-            if(estado[reyY-1][reyX-1] == 112)
-                return true;
-
-        if(reyX<5 && reyY>0)
-            if(estado[reyY-1][reyX+1] == 112)
-                return true;
-    }
-    else
-    {
-        if(reyX>0 && reyY<5)
-            if(estado[reyY+1][reyX-1] == 80)
-                return true;
-
-        if(reyX<5 && reyY<5)
-            if(estado[reyY+1][reyX+1] == 80)
-                return true;
-    }
-
-    //Otro rey
-    for (int i = 0; i < 8; ++i)
-    {
-        xx = reyX + dxRey[i];
-        yy = reyY + dyRey[i];
-
-        if(xx<0 || xx>5 || yy<0 || yy>5)
-            continue;
-        else
-            if(estado[yy][xx] == (75+delta))
-                return true;
-    }
-
-    return respuesta;
 }
 
 void Ventana::crearEstadoDeArreglos()
@@ -357,21 +216,19 @@ void Ventana::newGame()
     ui->pushButtonHard->setHidden(false);
 }
 
-void Ventana::gameEasy()
+void Ventana::game(int nivel) // Facil=0, Medio=1, Dificil=2
 {
+    minimax = new MiniMax(nivel);
     crearTablero();
-    cout<<"Empieza un juego fácil"<<endl;
+
+    if(nivel==0)
+        cout<<"Empieza un juego fácil"<<endl;
+
+    if(nivel==1)
+        cout<<"Empieza un juego intermedio"<<endl;
+
+    if(nivel==2)
+        cout<<"Empieza un juego difícil"<<endl;
 }
 
-void Ventana::gameMedium()
-{
-    crearTablero();
-    cout<<"Empieza un juego intermedio"<<endl;
-}
-
-void Ventana::gameHard()
-{
-    crearTablero();
-    cout<<"Empieza un juego difícil"<<endl;
-}
 
