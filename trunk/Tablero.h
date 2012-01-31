c9917322e0552307874c0aaaa9e50389b522e2b0
vialrogo@gmail.com <vialrogo@gmail.com@d3711144-6350-d657-4f93-78f5ff1c1195>
2012-01-31 02:47:00 +0000

    Animaciones terminadas :D :D :D

--- trunk/Tablero.h
+++ trunk/Tablero.h
@@ -22,19 +22,23 @@ public:
     Tablero();
     void crearFichas(int* posBlancasX, int* posBlancasY, int* posNegrasX, int* posNegrasY);
     void pintarCuadricula();
-    void iniciarAnimacion(int ficha, bool color, int xIni, int yIni, int xFin, int yFin);
+    void iniciarAnimacion(int ficha, bool color, int xIni, int yIni, int xFin, int yFin, bool tocaElimiarLuego);
     void mousePressEvent(QGraphicsSceneMouseEvent* event);
     void seleccionarFicha(int xFicha, int yFicha);
     void seleccionarFicha(int xFicha, int yFicha, int xAnterior, int yAnterior);
-    void eliminarFicha(int ficha, bool color);
+    void eliminarFicha(int ficha, bool color, int X, int Y);
 
 private:
     //Variables Animación
     int tiempo;
     int ficha_global;
     bool color_global;
+    int ficha_global_eliminar;
+    bool color_global_eliminar;
     int tipoMovimiento;
     int signo;
+    int tiempoAnimacion;
+    bool tocaEliminarLuego_global;
 
     //Variables Tablero
     int deltaX;
@@ -50,13 +54,16 @@ private:
     Ficha** imagenesFichasBlancasMinisNormal;
     Ficha** imagenesFichasNegrasMinisNormal;
     QTimer *timer;
+    QTimer *timerEliminar;
     void moverFicha(int ficha, bool color, int dx, int dy);
 
 private slots:
     void animar();
+    void eliminarImagenFicha();
 
 signals:
     void terminoAnimacion();
+    void empezarEliminar();
     void celdaCliqueada(int X, int Y);
 };
 
